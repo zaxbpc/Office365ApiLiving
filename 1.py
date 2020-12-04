3e6e09010882bb3c364835caf9af3e7869fc86e2
zaxbpc <63099453+zaxbpc@users.noreply.github.com>
2020-12-04 08:57:28 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 import requests as req
-import json,sys,time,random
+import json,sys,time
 #先注册azure应用,确保应用有以下权限:
 #files:	Files.Read.All、Files.ReadWrite.All、Sites.Read.All、Sites.ReadWrite.All
 #user:	User.Read.All、User.ReadWrite.All、Directory.Read.All、Directory.ReadWrite.All
@@ -28,6 +28,8 @@ def gettoken(refresh_token):
     jsontxt = json.loads(html.text)
     refresh_token = jsontxt['refresh_token']
     access_token = jsontxt['access_token']
+    with open(path, 'w+') as f:
+        f.write(refresh_token)
     return access_token
 def main():
     fo = open(path, "r+")
@@ -40,7 +42,6 @@ def main():
     'Authorization':access_token,
     'Content-Type':'application/json'
     }
-    print('此次运行开始时间为 :', localtime)
     try:
         if req.get(r'https://graph.microsoft.com/v1.0/me/drive/root',headers=headers).status_code == 200:
             num1+=1
@@ -60,7 +61,7 @@ def main():
         if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',headers=headers).status_code == 200:
             num1+=1
             print('6调用成功'+str(num1)+'次')    
-        if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',headers=headers).status_code == 200:
+        if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/Inbox/messages/delta',headers=headers).status_code == 200:
             num1+=1
             print('7调用成功'+str(num1)+'次')
         if req.get(r'https://graph.microsoft.com/v1.0/me/drive/root/children',headers=headers).status_code == 200:
@@ -75,10 +76,10 @@ def main():
         if req.get(r'https://graph.microsoft.com/v1.0/me/outlook/masterCategories',headers=headers).status_code == 200:
             num1+=1
             print('10调用成功'+str(num1)+'次')
+            print('此次运行结束时间为 :', localtime)
     except:
         print("pass")
         pass
-for _ in range(4): 
-    for i in range(random.randint(600,1200),0,-1):
-        time.sleep(1)
+for _ in range(3):
     main()
+    
